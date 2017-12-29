c28517e4f7fc9fc78cbda889d48ea533f1fa3045
jinqiucheng <jinqiucheng@autohome.com.cn>
2017-12-29 11:11:01 +0800

    收集依赖项

--- dreamspace/settings.py
+++ dreamspace/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!-a(h!axn2nx0gsifmdz@t6ag-xl7h37#&ul!9p%7@#5nc-1hf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.dreamtracer.top']
 
 
 # Application definition
@@ -154,9 +154,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATIC_URL = '/static/'
+# 加入下面的配置
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
