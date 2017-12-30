13bcddf73d7ce9cb72e61e851adc8ece9d186c9c
jinqiucheng <jinqiucheng@autohome.com.cn>
2017-12-31 00:15:19 +0800

    1

--- dreamspace/settings.py
+++ dreamspace/settings.py
@@ -160,7 +160,7 @@ STATIC_URL = '/static/'
 # 加入下面的配置
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    # os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'blog/static'),
 ]
 
